9fa7ec21b3db788402345b58c5df76a35d39bcd2
Oddrne <odd.a.fosse@ntnu.no>
2026-04-09 10:52:33 +0200

    Removed the old remnants of size 200

--- Convolutional_AE.py
+++ Convolutional_AE.py
@@ -95,7 +95,7 @@ class ClusteringLayer(nn.Module):
 class DCEC(nn.Module):
     """
     Convolutional Autoencoder with a low-dimensional embedded layer z.
-    Assumes input images of shape (1, 200, 200), e.g. FC matrices.
+    Assumes input images of shape (1, 400, 400), e.g. FC matrices.
     """
     def __init__(self, cfg: DCECConfig):
         super().__init__()
@@ -113,9 +113,9 @@ class DCEC(nn.Module):
         self.enc_conv1 = nn.Conv2d(conv_layers_sizes[0], conv_layers_sizes[1], kernel_size=6, stride=2, padding=2)   # 400x400x1 -> 200x200x32
         self.enc_conv2 = nn.Conv2d(conv_layers_sizes[1], conv_layers_sizes[2], kernel_size=6, stride=2, padding=2)  # 200x200x32 -> 100x100x64
         self.enc_conv3 = nn.Conv2d(conv_layers_sizes[2], conv_layers_sizes[3], kernel_size=6, stride=2, padding=2) # 100x100x64 -> 50x50x128
-        self.enc_conv4 = nn.Conv2d(conv_layers_sizes[3], conv_layers_sizes[4], kernel_size=5, stride=3, padding=3) # 50x50x128 -> 25x25x256
-        self.enc_conv5 = nn.Conv2d(conv_layers_sizes[4], conv_layers_sizes[4], kernel_size=4, stride=2, padding=1) # 25x25x256 -> 12x12x256
-        self.enc_conv6 = nn.Conv2d(conv_layers_sizes[4], conv_layers_sizes[4], kernel_size=5, stride=3, padding=1) # 12x12x256 -> 6x6x256
+        self.enc_conv4 = nn.Conv2d(conv_layers_sizes[3], conv_layers_sizes[4], kernel_size=5, stride=3, padding=3) # 50x50x128 -> 18x18x256
+        self.enc_conv5 = nn.Conv2d(conv_layers_sizes[4], conv_layers_sizes[4], kernel_size=4, stride=2, padding=1) # 18x18x256 -> 9x9x256
+        self.enc_conv6 = nn.Conv2d(conv_layers_sizes[4], conv_layers_sizes[4], kernel_size=5, stride=3, padding=1) # 9x9x256 -> 3x3x256
 
         self.flatten = nn.Flatten() # 1x2304
         self.fc_enc = nn.Linear(256 * 3 * 3, self.latent_dim) # 2304 -> latent_dim = 10
@@ -138,12 +138,12 @@ class DCEC(nn.Module):
         # Decoder
         self.fc_dec = nn.Linear(self.latent_dim, 256 * 3 * 3) # latent_dim -> 2304
 
-        self.dec_deconv1 = nn.ConvTranspose2d(conv_layers_sizes[-1], conv_layers_sizes[-1], kernel_size=5, stride=3, padding=1, output_padding=0)  # 3x3x256 -> 6x6x256
-        self.dec_deconv2 = nn.ConvTranspose2d(conv_layers_sizes[-1], conv_layers_sizes[-1], kernel_size=4, stride=2, padding=1, output_padding=0)  # 6x6x256 -> 12x12x256
-        self.dec_deconv3 = nn.ConvTranspose2d(conv_layers_sizes[-1], conv_layers_sizes[-2], kernel_size=5, stride=3, padding=3, output_padding=0)  # 12x12x256 -> 25x25x128
-        self.dec_deconv4 = nn.ConvTranspose2d(conv_layers_sizes[-2], conv_layers_sizes[-3], kernel_size=6, stride=2, padding=2, output_padding=0)  # 25x25x128 -> 50x50x64 (if needed)
-        self.dec_deconv5 = nn.ConvTranspose2d(conv_layers_sizes[-3], conv_layers_sizes[-4], kernel_size=6, stride=2, padding=2, output_padding=0)  # 50x50x64 -> 100x100x32 (if needed)
-        self.dec_deconv6 = nn.ConvTranspose2d(conv_layers_sizes[-4], conv_layers_sizes[-5], kernel_size=6, stride=2, padding=2, output_padding=0)  # 100x100x32 -> 200x200x1 (if needed)
+        self.dec_deconv1 = nn.ConvTranspose2d(conv_layers_sizes[-1], conv_layers_sizes[-1], kernel_size=5, stride=3, padding=1, output_padding=0)  # 3x3x256 -> 9x9x256
+        self.dec_deconv2 = nn.ConvTranspose2d(conv_layers_sizes[-1], conv_layers_sizes[-1], kernel_size=4, stride=2, padding=1, output_padding=0)  # 9x9x256 -> 18x18x256
+        self.dec_deconv3 = nn.ConvTranspose2d(conv_layers_sizes[-1], conv_layers_sizes[-2], kernel_size=5, stride=3, padding=3, output_padding=0)  # 18x18x256 -> 25x25x128
+        self.dec_deconv4 = nn.ConvTranspose2d(conv_layers_sizes[-2], conv_layers_sizes[-3], kernel_size=6, stride=2, padding=2, output_padding=0)  # 25x25x128 -> 50x50x64 
+        self.dec_deconv5 = nn.ConvTranspose2d(conv_layers_sizes[-3], conv_layers_sizes[-4], kernel_size=6, stride=2, padding=2, output_padding=0)  # 50x50x64 -> 100x100x32 
+        self.dec_deconv6 = nn.ConvTranspose2d(conv_layers_sizes[-4], conv_layers_sizes[-5], kernel_size=6, stride=2, padding=2, output_padding=0)  # 100x100x32 -> 200x200x1 
 
 
     def encode(self, x: torch.Tensor) -> torch.Tensor:
@@ -303,7 +303,7 @@ def initialize_cluster_centers(
 
     return y_pred
 
-def make_upper_triangle_mask(n=200, include_diagonal=False, device='cpu'):
+def make_upper_triangle_mask(n=400, include_diagonal=False, device='cpu'):
     if include_diagonal:
         mask = torch.triu(torch.ones(n, n, dtype=torch.bool, device=device), diagonal=0)
     else:
@@ -405,7 +405,7 @@ def train_dcec(
         L_c = KL(P || Q)
     """
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
-    mask = make_upper_triangle_mask(n=200, include_diagonal=False, device=device).unsqueeze(0)  # Shape (1, 200, 200)
+    mask = make_upper_triangle_mask(n=400, include_diagonal=False, device=device).unsqueeze(0)  # Shape (1, 400, 400)
 
     model.to(device)
     
